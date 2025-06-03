367813d48a3c6777b12d6ae30058923324c2efbe
nndrrroo <mahendraarya509@gmail.com>
2025-06-03 19:22:27 +0700

    SCRUM-55 Penyelesaian PBI 28

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -92,7 +92,7 @@
 
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                     @csrf
-                                </form>
+                                </form> 
                             </div>
                         </li>
                     @endguest
